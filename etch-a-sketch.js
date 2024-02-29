62fa4ab32bd9975deef784079de6f5a8c2d249d1
Rana <Ranaahmad51@gmail.com>
2024-02-29 16:24:39 -0500

    update js, html and css files to produce a working etch a sketch page with grid resizing and grid clearing

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,16 +1,33 @@
 const container = document.getElementById("etchContainer");
 
+function buttonPrompt(){
+    let size = prompt("Enter a number between 0 and 100");
 
+    if (size > 0 && size < 101) {
+        container.replaceChildren();
+        startGrid(size);
+        return size;
+    }
+    else if (size < 0 || size > 100 || typeof size === 'string') {
+        alert("Value must be between 0 and 100");
+        return;
+    }
 
+}
+function startGrid(size){
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-function makeGrid(rows,cols){
-    container.style.setProperty('--grid-rows',rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+    for (c = 0; c < (size**2); c++) {
         let cell = document.createElement("div");
-        cell.innerText =(c + 1);
         container.appendChild(cell).className = "grid-item";
+        cell.addEventListener('mouseover', () => {
+            cell.style.background = 'black';
+        })
     };
 };
-
-makeGrid(16,16);
+function clearGrid(){
+    container.replaceChildren();
+    location.reload();
+}
+startGrid(16);
